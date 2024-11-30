7ff98dfc1e18b7c20376f9d27d7615e4921571cf
Kumari Nishita <nishitsingh75@gmail.com>
2024-11-30 19:39:17 +0530

    Initial commit

---
+++ Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Students.Models
+{
+    public class Class
+    {
+        [Key]
+        public int Id {  get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+
+        [MaxLength(100)]
+        public string? Description { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Students.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id {  get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? LastName { get; set; }
+        [Required]
+        [MaxLength(10)]
+        [RegularExpression(@"^\d{10}$", ErrorMessage ="PhoneNumber must be 10 digits.")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? EmailId { get; set; }
+        public ICollection<int> ClassIds { get; set; }
+
+
+    }
+}
